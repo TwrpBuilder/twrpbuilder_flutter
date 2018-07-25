4348da46cfa1d88ace362c4d0b58c217cadd35ff
Amit Khairnar <amitkhairnar44@gmail.com>
2018-07-25 19:48:11 +0530

    Fix implementation in Settings

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,33 +1,50 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
 class SettingsPage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return new SettingsPageState();
-  }
+  _SettingsPageState createState() => new _SettingsPageState();
 }
 
-class SettingsPageState extends State<SettingsPage> {
-  List<Widget> _listItems = [
-    ListTile(
-      title: Text("Language"),
-      onTap: (){},
-    ),
-    ListTile(
-      title: Text("Check for update"),
-      onTap: () {},
-    ),
-    CheckboxListTile(
-      value: true,
-      onChanged: (bool value) {
-        print(value);
-      },
-      title: Text("Disable Notification"),
-    )
-  ];
+class _SettingsPageState extends State<SettingsPage> {
+
+  static bool defaultValue = null ?? false;
+  static Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  Future<bool> _notification;
+
+  Future<Null> _loadPrefs() async {
+    final SharedPreferences prefs = await _prefs;
+    final bool notification = (prefs.getBool('notification') ?? false);
+
+    setState(() {
+      _notification = prefs.setBool("notification", notification).then((bool success) {
+        return notification;
+      });
+    });
+  }
+
+  static Future<Null> _savePrefs(bool value) async {
+    final SharedPreferences prefs = await _prefs;
+    prefs.setBool("notification", value);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _notification = _prefs.then((SharedPreferences prefs){
+      setState(() {
+        defaultValue = prefs.getBool("notification") ?? false;
+      });
+      print(defaultValue);
+      return defaultValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,10 +60,36 @@ class SettingsPageState extends State<SettingsPage> {
         centerTitle: true,
         backgroundColor: Colors.white,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: _listItems,
-      ),
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text("Language"),
+            onTap: (){},
+          ),
+          ListTile(
+            title: Text("Check for update"),
+            onTap: () {},
+          ),
+          CheckboxListTile(
+            value: defaultValue,
+            onChanged: (bool value) {
+              setState(() {
+                defaultValue = value;
+                _savePrefs(value);
+
+                if (value == true) {
+                  
+                } else {
+
+                }
+              });
+            },
+            title: Text("Disable Notification"),
+          )
+        ],
+      )
     );
   }
 }
+
+
