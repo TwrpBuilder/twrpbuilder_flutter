c7fd7f75b4b09769450de433a378239fd0aa9cda
Amit Khairnar <amitkhairnar44@gmail.com>
2018-08-01 15:54:12 +0530

    Implement Contributors page

--- lib/fragments/contact_fragment.dart
+++ lib/fragments/contact_fragment.dart
@@ -6,15 +6,12 @@ import '../app_constants.dart';
 import '../json_translations.dart';
 
 class ContactFragment extends StatefulWidget {
-
   @override
   _ContactFragmentState createState() => new _ContactFragmentState();
 }
 
 class _ContactFragmentState extends State<ContactFragment> {
-
   Future<Null> _launchURL(String url) async {
-
     if (await canLaunch(url)) {
       await launch(url, forceWebView: false);
     } else {
@@ -37,8 +34,15 @@ class _ContactFragmentState extends State<ContactFragment> {
                         title: Row(
                           children: <Widget>[
                             Icon(Icons.public),
-                            Padding(padding: EdgeInsets.only(left: 5.0, right: 5.0)),
-                            Text(Translations.of(context).text('official_website'), style: TextStyle(color: Colors.black, fontSize: 16.0))
+                            Padding(
+                                padding:
+                                    EdgeInsets.only(left: 5.0, right: 5.0)),
+                            Text(
+                                Translations
+                                    .of(context)
+                                    .text('official_website'),
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16.0))
                           ],
                         ),
                         onTap: () {
@@ -51,8 +55,12 @@ class _ContactFragmentState extends State<ContactFragment> {
                         title: Row(
                           children: <Widget>[
                             Icon(xda),
-                            Padding(padding: EdgeInsets.only(left: 5.0, right: 5.0)),
-                            Text(Translations.of(context).text('xda_thread'), style: TextStyle(color: Colors.black, fontSize: 16.0))
+                            Padding(
+                                padding:
+                                    EdgeInsets.only(left: 5.0, right: 5.0)),
+                            Text(Translations.of(context).text('xda_thread'),
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16.0))
                           ],
                         ),
                         onTap: () {
@@ -65,8 +73,12 @@ class _ContactFragmentState extends State<ContactFragment> {
                         title: Row(
                           children: <Widget>[
                             Icon(github_circle),
-                            Padding(padding: EdgeInsets.only(left: 5.0, right: 5.0)),
-                            Text(Translations.of(context).text('source'), style: TextStyle(color: Colors.black, fontSize: 16.0))
+                            Padding(
+                                padding:
+                                    EdgeInsets.only(left: 5.0, right: 5.0)),
+                            Text(Translations.of(context).text('source'),
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16.0))
                           ],
                         ),
                         onTap: () {
@@ -79,8 +91,15 @@ class _ContactFragmentState extends State<ContactFragment> {
                         title: Row(
                           children: <Widget>[
                             Icon(telegram),
-                            Padding(padding: EdgeInsets.only(left: 5.0, right: 5.0)),
-                            Text(Translations.of(context).text('telegram_support'), style: TextStyle(color: Colors.black, fontSize: 16.0))
+                            Padding(
+                                padding:
+                                    EdgeInsets.only(left: 5.0, right: 5.0)),
+                            Text(
+                                Translations
+                                    .of(context)
+                                    .text('telegram_support'),
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16.0))
                           ],
                         ),
                         onTap: () {
@@ -93,8 +112,12 @@ class _ContactFragmentState extends State<ContactFragment> {
                         title: Row(
                           children: <Widget>[
                             Icon(Icons.bug_report),
-                            Padding(padding: EdgeInsets.only(left: 5.0, right: 5.0)),
-                            Text(Translations.of(context).text('report_a_bug'), style: TextStyle(color: Colors.black, fontSize: 16.0))
+                            Padding(
+                                padding:
+                                    EdgeInsets.only(left: 5.0, right: 5.0)),
+                            Text(Translations.of(context).text('report_a_bug'),
+                                style: TextStyle(
+                                    color: Colors.black, fontSize: 16.0))
                           ],
                         ),
                         onTap: () {
@@ -108,90 +131,7 @@ class _ContactFragmentState extends State<ContactFragment> {
                 )
               ],
             ),
-          )
-      ),
+          )),
     );
   }
-
 }
-
-//new Column(
-//mainAxisAlignment: MainAxisAlignment.start,
-//children: <Widget>[
-//new Row(
-//mainAxisAlignment: MainAxisAlignment.start,
-//children: <Widget>[
-//new Padding(
-//padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0, bottom: 8.0),
-//child: new FlatButton.icon(
-//onPressed: () => setState(() {
-//_launchURL(OfficialWebsite);
-//}),
-//icon: Icon(Icons.public, size: 30.0,),
-//label: Text("Official Website", style: TextStyle(color: Colors.black, fontSize: 16.0),)
-//),
-//)
-//],
-//),
-//new Row(
-//mainAxisAlignment: MainAxisAlignment.start,
-//children: <Widget>[
-//new Padding(
-//padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-//child: new FlatButton.icon(
-//onPressed: () => setState(() {
-//_launchURL(XdaThread);
-//}),
-//icon: Icon(xda, size: 30.0,),
-//label: Text("XDA Thread", style: TextStyle(color: Colors.black, fontSize: 16.0),)
-//),
-//)
-//],
-//),
-//new Row(
-//mainAxisAlignment: MainAxisAlignment.start,
-//children: <Widget>[
-//new Padding(
-//padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-//child: new FlatButton.icon(
-//onPressed: () => setState(() {
-//_launchURL(GithubSource);
-//}),
-//icon: Icon(github_circle, size: 30.0,),
-//label: Text("Source", style: TextStyle(color: Colors.black, fontSize: 16.0),)
-//),
-//)
-//],
-//),
-//new Row(
-//mainAxisAlignment: MainAxisAlignment.start,
-//children: <Widget>[
-//new Padding(
-//padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-//child: new FlatButton.icon(
-//onPressed: () => setState(() {
-//_launchURL(TGramSupport);
-//}),
-//icon: Icon(telegram, size: 30.0,),
-//label: Text("Telegram Support", style: TextStyle(color: Colors.black, fontSize: 16.0),)
-//),
-//)
-//],
-//),
-//new Row(
-//mainAxisAlignment: MainAxisAlignment.start,
-//children: <Widget>[
-//new Padding(
-//padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 8.0, bottom: 8.0),
-//child: new FlatButton.icon(
-//onPressed: () => setState(() {
-//_launchURL(ReportIssue);
-//}),
-//icon: Icon(Icons.bug_report , size: 30.0,),
-//label: Text("Report a bug", style: TextStyle(color: Colors.black, fontSize: 16.0),)
-//),
-//)
-//],
-//)
-//],
-//)
--- lib/fragments/contributers_fragment.dart
+++ lib/fragments/contributers_fragment.dart
@@ -1,6 +1,68 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
 
-class ContributeFragment extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../app_constants.dart';
+import '../model/contributors_model.dart';
+import '../json_translations.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class ContributeFragment extends StatefulWidget {
+  @override
+  _ContributeFragmentPage createState() => new _ContributeFragmentPage();
+}
+
+class _ContributeFragmentPage extends State<ContributeFragment> {
+  List<ContributorsModel> contributorsModel = [];
+  @override
+  void initState() {
+    super.initState();
+    loadContributorsData();
+  }
+
+  Future<Null> _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceWebView: false);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  loadContributorsData() async {
+    var response = await http
+        .get(URL_CONTRIBUTORS, headers: {"Accept": "application/json"});
+    if (response.statusCode == 200) {
+      String responseBody = response.body;
+      var jsonBody = json.decode(responseBody);
+      for (var data in jsonBody) {
+        contributorsModel.add(new ContributorsModel(
+            data['login'],
+            data['id'],
+            data['node_id'],
+            data['avatar_url'],
+            data['gravatar_id'],
+            data['url'],
+            data['html_url'],
+            data['followers_url'],
+            data['following_url'],
+            data['gists_url'],
+            data['starred_url'],
+            data['subscriptions_url'],
+            data['organizations_url'],
+            data['repos_url'],
+            data['events_url'],
+            data['received_events_url'],
+            data['type'],
+            data['site_admin'],
+            data['contributions']));
+      }
+      setState(() {});
+    } else {
+      print("Failed to load data!");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -10,15 +72,60 @@ class ContributeFragment extends StatelessWidget {
           child: Scaffold(
             body: TabBarView(
               children: [
-                Center(
-                  child: Text(
-                    "Details about contributers will be here",
-                    textAlign: TextAlign.center,
-                  ),
-                )
+                contributorsModel.length == 0
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : updateUI()
               ],
             ),
           )),
     );
+  }
+
+  Widget updateUI() {
+    return ListView.builder(
+        itemCount: contributorsModel.length,
+        itemBuilder: (_, index) {
+          return ListTile(
+            title: Padding(
+              padding: EdgeInsets.only(top: 10.0, bottom: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    width: 50.0,
+                    height: 50.0,
+                    decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(
+                                "${contributorsModel[index].avatar_url}"))),
+                  ),
+                  Padding(padding: EdgeInsets.only(left: 5.0, right: 5.0)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(contributorsModel[index].login,
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w500)),
+                      Text(
+                          'Contributions:  ${contributorsModel[index].contributions.toString()}',
+                          style: TextStyle(color: Colors.blue, fontSize: 15.0))
+                    ],
+                  )
+                ],
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                _launchURL(contributorsModel[index].html_url);
+              });
+            },
+          );
+        });
   }
 }
